c91b88c231455cabc24a53473beeb12b4e537a1e
othree <othree@gmail.com>
2017-03-06 16:44:49 +0800

    1.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_zenedit do
   name 'Redmine Zen Edit plugin'
   author 'RedmineCRM'
   description 'This is a Zen edit plugin for Redmine'
-  version '0.0.2'
+  version '1.0'
   url 'http://redminecrm.com'
   author_url 'mailto:support@redminecrm.com'
 end
